69aca5d3d12381d2f9b675632561640988b61010
tuan <tuanwwdan3@gmail.com>
2021-03-15 20:30:25 +0700

    mer

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
     return $request->user()03953853958;
 });
