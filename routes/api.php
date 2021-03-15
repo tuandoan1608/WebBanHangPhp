afdacfba522a7a4d2ce7f1542bfc347498c41599
tuan <tuanwwdan3@gmail.com>
2021-03-15 20:22:22 +0700

    đ

--- routes/api.php
+++ routes/api.php
@@ -14,8 +14,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
-    fdsfsdfsfsfsf
+    return $request->user();
 });
-
-
-day là branch demo
