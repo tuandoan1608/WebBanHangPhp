d96f7235fc8610a3adfc971ae0b3df99df6ee9e0
tuandoan1608 <57795678+tuandoan1608@users.noreply.github.com>
2021-03-23 12:58:21 +0700

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -14,5 +14,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user()03953853958;
+    return $request->user();
 });
